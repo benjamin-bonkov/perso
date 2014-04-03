c79fd4e4a1f60e4406b5d9ee7a887940043dc924
benjamin <benjamin@sooyoos.com>
2014-04-03 17:27:09 +0200

    cible ie11/suppr index nul/cookie

---
+++ php/cookie.php
@@ -0,0 +1,17 @@
+<?php if(!isset($_COOKIE['cookie-babybel']) || !$_COOKIE['cookie-babybel']): ?>
+    <div id="cookie">
+        <p>
+	        Les cookies assurent le bon fonctionnement de notre site. En utilisant ce dernier, vous acceptez l'utilisation des cookies. 
+	        <a href="<?php echo $this->baseUrl('/vie-privee#cookies'); ?>">En savoir +</a>
+	        <button class="btn red close">OK</button>
+        </p>
+    </div>
+<?php endif; ?>
+<script>
+	$("#cookie .close").click(function(){
+		$("#cookie").fadeOut().remove();
+		var date = new Date();
+		date.setYear(date.getUTCFullYear() + 1);
+		document.cookie="cookie-babybel=true expires="+ date.toUTCString() +"; path=/";
+	});
+</script>
