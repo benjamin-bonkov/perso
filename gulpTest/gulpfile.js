ca984131e9552e1af65db69c0bb0b2a4678d0abb
benjamin <benjamin@sooyoos.com>
2014-05-30 16:49:59 +0200

    bug anim from bottom gulp concat/min css

--- gulpTest/gulpfile.js
+++ gulpTest/gulpfile.js
@@ -2,32 +2,45 @@
 var gulp = require('gulp'); 
 
 // Include Our Plugins
-var jshint = require('gulp-jshint');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
+var jshint = require('gulp-jshint')
+,	concat = require('gulp-concat')
+,	uglify = require('gulp-uglify')
+,	rename = require('gulp-rename')
+,	concatCss = require('gulp-concat-css')
+,	minifyCSS = require('gulp-minify-css');
 
 // Lint Task
 gulp.task('lint', function() {
-    return gulp.src('public/js/*.js')
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'));
+	return gulp.src('public/js/*.js')
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'));
 });
 
 // Concatenate & Minify JS
 gulp.task('scripts', function() {
-    return gulp.src('public/js/**/*.js')
-        .pipe(concat('all.js'))
-        .pipe(gulp.dest('public/dist'))
-        .pipe(rename('all.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('public/dist'));
+	return gulp.src('public/js/**/*.js')
+		.pipe(concat('all.js'))
+		.pipe(gulp.dest('public/dist/js'))
+		.pipe(rename('all.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('public/dist/js'));
+});
+
+// Concatenate & Minify CSS
+gulp.task('css', function () {
+	return gulp.src('public/css/**/*.css')
+		.pipe(concatCss("css/style.css"))
+		.pipe(gulp.dest('public/dist/'))
+		.pipe(rename("/css/style.min.css"))
+		.pipe(minifyCSS({}))
+		.pipe(gulp.dest('public/dist/'));
 });
 
 // Watch Files For Changes
 gulp.task('watch', function() {
-    gulp.watch('public/js/**/*.js', ['lint', 'scripts']);
+	gulp.watch('public/js/**/*.js', ['lint', 'scripts']);
+	gulp.watch('public/css/**/*.css', ['css']);
 });
 
 // Default Task
-gulp.task('default', ['lint', 'scripts', 'watch']);
+gulp.task('default', ['lint', 'scripts', 'css', 'watch']);
